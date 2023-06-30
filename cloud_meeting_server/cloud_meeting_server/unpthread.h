59dda2864650b2942bcb1a3195367281fc7c73d0
kakaacat <1912445179@qq.com>
2023-06-30 14:59:46 +0800

    [add]

--- cloud_meeting_server/cloud_meeting_server/unpthread.h
+++ cloud_meeting_server/cloud_meeting_server/unpthread.h
@@ -6,7 +6,7 @@
 #include <pthread.h>
 #include "unp.h"
 
-typedef void * (THREAD_FUNC) (void *);
+typedef void * (THREAD_FUNC) (void *);	//定义一个函数，void *是一种无类型指针
 
 typedef struct
 {
@@ -30,7 +30,8 @@ typedef struct Room //single
 	Room(int n)
 	{
 		navail = n;
-		pptr = (Process*)Calloc(n, sizeof(Process));
+		pptr = (Process*)Calloc(n, sizeof(Process));	/*C 库函数 void *calloc(size_t nitems, size_t size) 分配所需的内存空间，并返回一个指向它的指针。
+														malloc 和 calloc 之间的不同点是，malloc 不会设置内存为零，而 calloc 会设置分配的内存为零。*/
 		lock = PTHREAD_MUTEX_INITIALIZER;
 	}
 }Room;
@@ -53,7 +54,8 @@ void	Pthread_cond_wait(pthread_cond_t*, pthread_mutex_t*);
 void	Pthread_cond_timedwait(pthread_cond_t*, pthread_mutex_t*,
 	const struct timespec*);
 
-void	Pthread_key_create(pthread_key_t*, void (*)(void*));
+void	Pthread_key_create(pthread_key_t*, void (*)(void*)); /*void (*)(void*)是一个抽象出来的函数指针的模型 ，用来为专门应对这一类事物的复杂交互提供便利， 
+															 比如有了类型就能声“名”出未初始化的对象用来做参数甚至为该类事物提供动态分配的没名字的对象 ， 以及批量操作对象*/
 void	Pthread_setspecific(pthread_key_t, const void*);
 void	Pthread_once(pthread_once_t*, void (*)(void));
 
