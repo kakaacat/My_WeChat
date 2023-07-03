c5f5cb52e99885a68d272cc97b4bd98d42924360
kakaacat <1912445179@qq.com>
2023-07-03 17:09:31 +0800

    [add]

--- cloud_meeting_server/cloud_meeting_server/net.cpp
+++ cloud_meeting_server/cloud_meeting_server/net.cpp
@@ -154,5 +154,79 @@ void Listen(int fd, int backlog)
 
 int Tcp_connect(const char* host, const char* serv)
 {
+	int sockfd, n;
+	struct addrinfo hints, * res, * ressave;
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
+	if ((n = getaddrinfo(host, serv, &hints, &res)) != 0)
+	{
+		err_quit("tcp_connect error for %s, %s: %s", host, serv, gai_strerror(n));
+	}
+
+	ressave = res;
+	do
+	{
+		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sockfd < 0) continue;
+		if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0) break;
+		close(sockfd);
+	} while ((res = res->ai_next) != NULL);
+
+	if (res == NULL)
+	{
+		err_quit("tcp_connect error for %s,%s", host, serv);
+	}
+	freeaddrinfo(ressave);
+	return sockfd;
 }
+
+int Tcp_listen(const char* host, const char* service, socklen_t* addrlen)
+{
+	int listenfd, n;
+	const int on = 1;
+	struct addrinfo hints, * res, * ressave;
+	bzero(&hints, sizeof(struct addrinfo));
+
+	hints.ai_flags = AI_PASSIVE;	//设置了AI_PASSIVE标志，但没有指定主机名，那么return ipv6和ipv4通配地址
+	hints.ai_family = AF_UNSPEC;	//返回的是适用于指定主机名和服务名且适合任意协议族的地址
+	hints.ai_socktype = SOCK_STREAM;
+
+	char addr[MAXSOCKADDR];
+	if ((n = getaddrinfo(host, service, &hints, &res)) > 0)
+	{
+		err_quit("tcp listen error for %s %s: %s", host, service, gai_strerror(n));
+	}
+	ressave = res;
+	do
+	{
+		listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (listenfd < 0)
+		{
+			continue;	//error try again
+		}
+		Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+		if (bind(listenfd, res->ai_addr, res->ai_addrlen) == 0)
+		{
+			printf("server address: %s\n", Sock_ntop(addr, MaxSOCKADDR, res->ai_addr, res->ai_addrlen));
+			break;	//success
+		}
+		Close(listenfd);
+	} while ((res = res->ai_next) != NULL);
+	freeaddrinfo(ressave);	//free
+
+	if (res == NULL)
+	{
+		err_quit("tcp listen error for %s: %s", host, service);
+	}
+
+	Listen(listenfd, LISTENQ);
+	if (addrlen)
+	{
+		*addrlen = res->ai_addrlen;
+	}
+
+	return listenfd;
+}
+
